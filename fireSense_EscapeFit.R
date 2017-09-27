896181fb7d70b6fd9117c67d2341b588988c5457
JM <jean.d.marchal@gmail.com>
2017-09-27 09:43:20 -0400

    Batch updates 3

---
+++ fireSense_EscapeFit.R
@@ -0,0 +1,132 @@
+# Everything in this file gets sourced during simInit, and all functions and objects
+# are put into the simList. To use objects and functions, use sim$xxx.
+defineModule(sim, list(
+  name = "fireSense_EscapeFit",
+  description = "Fit statistical models that can be used to parameterize (calibrate) 
+                 the fire escape component of landscape fire models (e.g. fireSense).",
+  keywords = c("escape probability", "fire frequency", "logistic", "fireSense"),
+  authors = person("Jean", "Marchal", email = "jean.d.marchal@gmail.com", role = c("aut", "cre")),
+  childModules = character(0),
+  version = list(SpaDES.core = "0.1.0", fireSense_EscapeFit = "0.0.1"),
+  spatialExtent = raster::extent(rep(NA_real_, 4)),
+  timeframe = as.POSIXlt(c(NA, NA)),
+  timeunit = NA_character_, # e.g., "year",
+  citation = list("citation.bib"),
+  documentation = list("README.txt", "fireSense_EscapeFit.Rmd"),
+  reqdPkgs = list(),
+  parameters = rbind(
+    #defineParameter("paramName", "paramClass", default, min, max, "parameter description"),
+    defineParameter(name = "formula", class = "formula", default = ~1,
+                    desc = "a formula describing the model to be fitted."),
+    defineParameter(name = "data", class = "character", default = "dataFireSense_EscapeFit",
+                    desc = "a character vector indicating the names of objects 
+                            in the `simList` environment in which to look for 
+                            variables present in the model formula. `data` 
+                            objects should be data.frames. If variables are not
+                            found in `data` objects, they are searched in the
+                            `simList` environment."),
+    defineParameter(name = "initialRunTime", class = "numeric", default = start(sim),
+                    desc = "when to start this module? By default, the start 
+                    time of the simulation."),
+    defineParameter(name = "intervalRunModule", class = "numeric", default = NA, 
+                    desc = "optional. Interval between two runs of this module,
+                    expressed in units of simulation time."),
+    defineParameter(".useCache", "numeric", FALSE, NA, NA, "Should this entire module be run with caching activated? This is generally intended for data-type modules, where stochasticity and time are not relevant")
+  ),
+  inputObjects = expectsInput(
+    objectName = "dataFireSense_EscapeFit",
+    objectClass = "data.frame",
+    desc = "One or more objects of class data.frame in which to look for variables present in the model formula.",
+    sourceURL = NA_character_
+  ),
+  outputObjects = createsOutput(
+    objectName = "fireSense_EscapeFitted",
+    objectClass = "fireSense_EscapeFit",
+    desc = "A fitted model object of class fireSense_EscapeFit (inheriting from the class glm)."
+  )
+))
+
+## event types
+#   - type `init` is required for initialiazation
+
+doEvent.fireSense_EscapeFit = function(sim, eventTime, eventType, debug = FALSE) {
+
+  switch(
+    eventType,
+    init = { sim <- fireSense_EscapeFitInit(sim) },
+    run = { sim <- fireSense_EscapeFitRun(sim) },
+    save = {
+      # ! ----- EDIT BELOW ----- ! #
+      # do stuff for this event
+
+      # e.g., call your custom functions/methods here
+      # you can define your own methods below this `doEvent` function
+
+      # schedule future event(s)
+
+      # e.g.,
+      # sim <- scheduleEvent(sim, time(sim) + P(sim)$.saveInterval, "fireSense_EscapeFit", "save")
+
+      # ! ----- STOP EDITING ----- ! #
+    },
+    warning(paste("Undefined event type: '", current(sim)[1, "eventType", with = FALSE],
+                  "' in module '", current(sim)[1, "moduleName", with = FALSE], "'", sep = ""))
+  )
+  
+  invisible(sim)
+}
+
+## event functions
+#   - follow the naming convention `modulenameEventtype()`;
+#   - `modulenameInit()` function is required for initiliazation;
+#   - keep event functions short and clean, modularize by calling subroutines from section below.
+
+### template initialization
+fireSense_EscapeFitInit <- function(sim) {
+  
+  sim <- scheduleEvent(sim, eventTime = P(sim)$initialRunTime, current(sim)$moduleName, "run")
+  invisible(sim)
+  
+}
+
+fireSense_EscapeFitRun <- function(sim) {
+  
+  moduleName <- current(sim)$moduleName
+  
+  # Create a container to hold the data	
+  envData <- new.env(parent = envir(sim))
+  on.exit(rm(envData))
+  
+  # Load inputs in the data container
+  list2env(as.list(envir(sim)), envir = envData)
+  
+  for (x in P(sim)$data) {
+    
+    if (!is.null(sim[[x]])) {
+      
+      if (is.data.frame(sim[[x]])) {
+        
+        list2env(sim[[x]], envir = envData)
+        
+      } else stop(paste0(moduleName, "> '", x, "' is not a data.frame."))
+      
+    }
+    
+  }
+  
+  if (is.empty.model(P(sim)$formula))
+    stop(paste0(moduleName, "> The formula describes an empty model."))
+  
+  terms <- terms.formula(P(sim)$formula)
+  
+  if (!attr(terms, "response"))
+    stop(paste0(moduleName, "> Incomplete formula, the LHS is missing."))
+  
+  model <- glm(formula = P(sim)$formula, data = envData, family = "binomial")
+  class(model) <- c("fireSense_EscapeFit", class(model))
+  
+  sim$fireSense_EscapeFitted <- model
+  
+  invisible(sim)
+  
+}
